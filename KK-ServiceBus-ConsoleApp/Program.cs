4fe414c9594d6959cfb7745e2f15e7798b9acffc
kurniawan.kurniawan <kurniawan.kurniawan@carsales.com.au>
2014-06-04 20:23:16 +1000

    init

---
+++ KK-ServiceBus-ConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.ServiceBus;
+
+using Microsoft.ServiceBus.Messaging;
+
+
+namespace KK_ServiceBus_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString;
+            Console.WriteLine("Enter Service Bus Connection string:\n");
+            connectionString = Console.ReadLine();
+            var messageFactory = MessagingFactory.CreateFromConnectionString(connectionString);
+            var namespaceManager = NamespaceManager.CreateFromConnectionString(connectionString);
+
+            const string QueueName = "ServiceBusQueueSample";
+
+            if (namespaceManager == null)
+            {
+                Console.WriteLine("\nUnexpected Error: NamespaceManager is NULL");
+                return;
+            }
+
+            if (namespaceManager.QueueExists(QueueName))
+            {
+                namespaceManager.DeleteQueue(QueueName);
+            }
+            namespaceManager.CreateQueue(QueueName);
+        }
+    }
+}
